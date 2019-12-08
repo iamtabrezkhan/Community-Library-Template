53466ccbfe6bc15558930ad2014dd169101e9cfc
Shadab <shadab14meb346@gmail.com>
2019-12-08 20:31:55 +0530

    added folders from master to dev

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Navbar from "./shared/components/Navbar/Navbar";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+let onLogin = () => {
+	alert("login clicked");
+};
+let onSignup = () => {
+	alert("sign up clicked");
+};
+
+let onFacebookLogin = () => {
+	alert("facebook login clicked");
+};
+let onFacebookSignup = () => {
+	alert("facebook sign up clicked");
+};
+
+let onGoogleLogin = () => {
+	alert("google login clicked");
+};
+let onGoogleSignup = () => {
+	alert("google sign up clicked");
+};
 
 function App() {
 	return (
-		<div>
-			<h1>Community Library</h1>
-			<a href="//localhost:5000/api/v1/passportAuth/auth/facebook">
-				Facebook Login
-			</a>
-		</div>
+		<>
+			<Router>
+				<div className="main">
+					<Navbar />
+					<div className="content">
+						<Route path="/login" component={Login} />
+						<Route path="/signup" component={SignUp} />
+					</div>
+				</div>
+			</Router>
+		</>
 	);
 }
 
