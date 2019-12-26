906e8fcc11407154c1f053974d6c4469808094d2
Tabrez Khan <khan.t.29@gmail.com>
2019-12-26 15:08:40 +0530

    Landing page

--- src/shared/components/Banner/Banner.js
+++ src/shared/components/Banner/Banner.js
@@ -4,14 +4,11 @@ import { Link } from "react-router-dom";
 
 const Banner = () => {
 	return (
-		<div className={`${Classes.dark_overlay}`}>
-			<div className={`${Classes.banner}`}>
-				<div className={`${Classes.banner_content}`}>
-					<h1>Welcome to Community Library</h1>
-					<h3>Lend books it's risk free</h3>
-					<h3>Borrow books it's cost free</h3>
-				</div>
-			</div>
+		<div className={Classes.main}>
+			<h1>Welcome to Community Library</h1>
+			<h3>Lend books it's risk free</h3>
+			<h3>Borrow books it's cost free</h3>
+			<button>Get Started</button>
 		</div>
 	);
 };
--- src/shared/components/CommunityCard/CommunityCard.js
+++ src/shared/components/CommunityCard/CommunityCard.js
@@ -1,20 +1,40 @@
 import React from "react";
 import Classes from "./CommunityCard.module.css";
 import { Link } from "react-router-dom";
-const CommunityCard = props => {
+
+const CommunityCard = ({community}) => {
+
 	const joinClicked = () => {
 		alert("Join button clicked");
 	};
+
 	return (
-		<div className={Classes.card}>
-			<h1>Community Name Community egre erg</h1>
-			<h2>
-				<i className="fa fa-map-marker" aria-hidden="true"></i>
-				<span> </span>Location Location Location
-			</h2>
-			<div className={Classes.members}>
-				<p> 8 Members</p>
-				<button onClick={joinClicked}>Join</button>
+		<div className={Classes.main}>
+			<div className={Classes.img} style={
+				{
+					backgroundImage: community.imgSrc ? `url(${community.imgSrc})` : 'linear-gradient(45deg, var(--color-light-purple), var(--color-blue))'
+				}
+			}>
+				{
+					!community.imgSrc ? <div className={Classes.dummyImg}>{community.name[0].toUpperCase()}{community.name[1].toUpperCase()}</div> : null
+				}
+			</div>
+			<div className={Classes.body}>
+				<div className={Classes.name}>
+					{community.name}
+				</div>
+				<div className={Classes.locationInfo}>
+					<div className={Classes.icon}>
+						<i class="fa fa-map-marker" aria-hidden="true"></i>
+					</div>
+					<div className={Classes.location}>
+						{community.location}
+					</div>
+				</div>
+				<div className={Classes.members}>
+					<div>{community.members} members</div>
+					<button className={Classes.joinBtn}>Join</button>
+				</div>
 			</div>
 		</div>
 	);
